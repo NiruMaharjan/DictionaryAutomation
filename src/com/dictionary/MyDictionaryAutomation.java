9adc3abb1934c915615d91981546b2e2450178ce
maharjanniru <nmaharjan@deerhold.org>
2020-10-10 22:55:25 +0545

    Automate your word search

--- src/com/dictionary/MyDictionaryAutomation.java
+++ src/com/dictionary/MyDictionaryAutomation.java
@@ -1,15 +1,20 @@
 package com.dictionary;
 
+import com.pages.Dictionary;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class MyDictionaryAutomation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String baseUrl = "https://www.dictionary.com";
+        Dictionary dictionary = new Dictionary(driver);
 
-        String expectedTitle = "Dictionary.com | Meanings and Definitions of Words at Dictionary.com";
+        String expectedTitle =  dictionary.getTitle();
         String actualTitle = "";
         driver.get(baseUrl);
         actualTitle = driver.getTitle();
@@ -20,6 +25,25 @@ public class MyDictionaryAutomation {
         else{
             System.out.println("Test Failed");
         }
+//        File file = new File("word.txt");
+//        FileReader fr = new FileReader(file);
+//        BufferedReader reader =
+
+        String fileName = "C:\\dictionaryautomation\\DictionaryAutomation\\src\\com\\dictionary\\word.txt";
+
+       InputStream fis = new FileInputStream(fileName);
+             InputStreamReader isr = new InputStreamReader(fis,
+                     StandardCharsets.UTF_8);
+             BufferedReader br = new BufferedReader(isr);
+             String word = br.readLine();
+        System.out.println(word);
+
+
+
+       dictionary.setSearchWord(word);
+        br.close();
+
+
         //close fire fox
         driver.close();
 
--- src/com/test/TestDictionary.java
+++ src/com/test/TestDictionary.java
@@ -1,6 +1,45 @@
 package com.test;
 
+import com.pages.Dictionary;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.*;
+
 public class TestDictionary {
-    String driverPath = "D:\\chromedriver.exe";
+    WebDriver driver;
+    Dictionary dictionary;
+
+   // Dictionary dictionary = new Dictionary(driver);
+    @BeforeTest
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+         driver = new ChromeDriver();
+        String baseUrl = "https://www.dictionary.com";
+        driver.get(baseUrl);
+    }
+
+    @Test
+    public void test_Homme_Page() throws IOException {
+        dictionary = new Dictionary(driver);
+        Assert.assertTrue(dictionary.getTitle().equalsIgnoreCase("DICTIONARY.COM"));
+
+        BufferedReader br = new BufferedReader(new FileReader("C:\\dictionaryautomation\\DictionaryAutomation\\src\\com\\dictionary\\word.txt"));
+        String line ="";
+        while((line = br.readLine() )!=null) {
+            System.out.println(line);
+
+
+            dictionary.setSearchWord(line);
+            dictionary.clickSearchBar();
+            String meaning = dictionary.getMeaning();
+            System.out.println(meaning);
+            dictionary.clearSearchBar();
+        }
+    }
+
 
 }
